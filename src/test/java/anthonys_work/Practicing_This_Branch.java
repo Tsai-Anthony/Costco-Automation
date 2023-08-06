d3ce0224e1fae546f6a5e1c0b2248285a963e60c
Anthony <tsai96anthony@gmail.com>
2023-08-06 19:27:04 -0400

    testing

--- src/test/java/anthonys_work/Practicing_This_Branch.java
+++ src/test/java/anthonys_work/Practicing_This_Branch.java
@@ -5,6 +5,7 @@ public class Practicing_This_Branch {
 	public static void main(String[] args) {
 //hello if you are reading this!
 //just wanted to see if i made this master branch correctly and no errors hopefully
+		//new change
 	}
 
 }
