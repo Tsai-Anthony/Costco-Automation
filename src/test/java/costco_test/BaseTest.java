5a8617281a3c2c82166ab30fbf4bc3e342ba8b64
Anthony <tsai96anthony@gmail.com>
2023-08-25 01:12:53 -0400

    hw 2 done

--- src/test/java/costco_test/BaseTest.java
+++ src/test/java/costco_test/BaseTest.java
@@ -3,29 +3,27 @@ package costco_test;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BaseTest {
-	static WebDriver driver;
+    static WebDriver driver;
 
-	public static void setUpTest(String url) {
+    public static void setUpTest(String url) {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.manage().window().maximize();
+        driver.get(url);
+    }
 
-		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-		driver.manage().window().maximize();
-		driver.get(url);
-	}
+    public static void endTest() {
+        driver.quit();
+    }
 
-	public static void endTest() {
-		driver.close();
-		driver.quit();
-	}
-	public static void customWait(int seconds) {
-		try {
-			Thread.sleep(seconds *1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    public static WebElement customWait(ExpectedCondition<WebElement> expectedCondition) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        return wait.until(expectedCondition);
+    }
 }
